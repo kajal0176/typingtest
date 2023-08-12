8903006c8e149393f02116610d84f483dccfce6f
kajal kumari <kajalk.ug20.ece@nitp.ac.in>
2023-08-12 11:23:01 +0530

    .

--- index.js
+++ index.js
@@ -1,79 +1,76 @@
+//  (actualWords / totalTimeTaken) * 60;
 
-    let arr=[
-        "Push yourself, because no one else is going to do it for you.",
-  "Failure is the condiment that gives success its flavor.",
-  "Wake up with determination. Go to bed with satisfaction.",
-  "It's going to be hard, but hard does not mean impossible.",
-  "Learning never exhausts the mind.",
-  "The only way to do great work is to love what you do."
-    ];
-    let text='';
-    const msg=document.getElementById('msg');
-    const btn=document.getElementById('btn');
-    
-    const typewords=document.getElementById('mywords');
-    let startTime,endTime;
-    const playgame=()=>{
-        
-        let date=new Date();
-        startTime=date.getTime();
-        var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        document.getElementById('starttime').innerText=time;
-        btn.innerText="Done";
+const typing_ground = document.querySelector('#textarea');
+const btn = document.querySelector('#btn');
+const score = document.querySelector('#score');
+const show_sentence = document.querySelector('#showSentence');
+
+let startTime, endTime, totalTimeTaken;
+
+
+const sentences = ['The quick brown fox jumps over the lazy dog 1',
+    'The quick brown fox jumps over the lazy dog 2',
+    'The quick brown fox jumps over the lazy dog 3 ']
+
+
+
+const calculateTypingSpeed = (time_taken) => {
+    let  totalWords = typing_ground.value.trim();
+    let actualWords = totalWords === '' ? 0 : totalWords.split(" ").length;
+
+    if(actualWords !== 0) {
+        let typing_speed  =  (actualWords / time_taken) * 60;
+        typing_speed = Math.round(typing_speed);
+        score.innerHTML = `Your typing speed is ${typing_speed} words per minutes & you wrote ${actualWords} words & time taken ${time_taken} sec`;
+    }else{
+        score.innerHTML = `Your typing speed is 0 words per minutes & time taken ${time_taken} sec`;
     }
-    
-    const endgame=()=>{
-        let date=new Date();
-        endTime=date.getTime();
-        var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        document.getElementById('endtime').innerText=time;
-        let totaltime=((endTime-startTime)/1000);
-        console.log(totaltime);
-        let totalstr=typewords.value ;
-        let wordscount=wordcount(totalstr);
-        console.log(wordscount);
-        let speed=Math.round((wordscount/totaltime)*60);
-        let showmsg="your speed is "+speed+" words per minute";
-       
-        
-        showmsg+=countcorrect(msg.innerText,totalstr);
-        msg.innerText=showmsg;
+}
+
+
+const endTypingTest = () => {
+    btn.innerText = "Start";
+
+    let date = new Date();
+    endTime = date.getTime();
+//totaltime is in milisecond thats why we are dividing it byy 1000 so that it will be in second
+    totalTimeTaken = (endTime -startTime) / 1000;
+
+    // console.log(totalTimeTaken);
+
+    calculateTypingSpeed(totalTimeTaken);
+
+    show_sentence.innerHTML = "";
+    typing_ground.value = "";
+}
+
+
+//arrow function
+const startTyping = () => {
+    let randomNumber = Math.floor(Math.random() * sentences.length);
+    // console.log(randomNumber);
+    show_sentence.innerHTML = sentences[randomNumber];
+
+    let date = new Date();
+    startTime = date.getTime();
+
+    btn.innerText = "Done";
+}
+
+
+
+
+
+btn.addEventListener('click', () => {
+    switch (btn.innerText.toLowerCase()) {
+        case "start":
+            typing_ground.removeAttribute('disabled');
+            startTyping();
+            break;
+
+        case "done":
+            typing_ground.setAttribute('disabled' , 'true');
+            endTypingTest();
+            break;
     }
-    
-    const wordcount=(str)=>{
-        let totalword=str.split(" ").length;
-        return totalword;
-    }
-    const countcorrect=(str1,str2)=>{
-        let word1=str1.split(" ");
-        let word2=str2.split(" ");
-        let cnt=0;
-        word1.forEach(function(item,index){
-            if(item==word2[index]){
-                cnt++;
-            }
-        })
-        let errorw=(word2.length-cnt);
-        return (cnt+" correct out of "+word1.length+" words and total errors are "+errorw);
-    }
-    btn.addEventListener('click',function(){
-        if(this.innerText=='start'){
-            for(let i=0;i<10;i++){
-                text+=arr[Math.floor(Math.random()*arr.length)];
-                text+=" ";
-                document.getElementById('msg').innerText=text;
-            }
-            typewords.disabled=false;
-           
-            playgame();
-               }
-               else if(this.innerText=="Done"){
-                   typewords.disabled=true; 
-                   btn.innerText="start";
-                   endgame();
-                   
-               }
-           
-           
-    })
-    
+})
